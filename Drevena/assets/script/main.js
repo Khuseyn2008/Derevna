73c3373aa9b7a7298ecfe2f4b701a7a3bb39a975
Huseyn <149490134+Khuseyn2008@users.noreply.github.com>
2025-11-27 18:49:48 +0500

    Delete Drevena directory

--- Drevena/assets/script/main.js
+++
@@ -1,9 +0,0 @@
-const links = document.querySelectorAll('a');
-console.log(links);
-
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault(); // ← отменяем переход по ссылке
-      console.log('Клик по ссылке:', link.href);
-    });
-  });
